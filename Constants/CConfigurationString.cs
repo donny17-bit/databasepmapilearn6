5ca9528f6badfa9b2fe94b3415880427e9d06b87
donny <doniwahyu14@gmail.com>
2024-10-04 10:46:07 +0700

    create constan class configuration

---
+++ Constants/CConfigurationString.cs
@@ -0,0 +1,13 @@
+namespace databasepmapilearn6.Constans;
+
+public class CConfigurationString
+{
+    // jwt configuration
+    public const string JWT = "Jwt";
+    public const string JWT_KEY = "Jwt:Key";
+    public const string JWT_AUD = "Jwt:Audience";
+    public const string JWT_ISS = "Jwt:Issuer";
+
+    // email 
+    public const string Email = "EmailConfiguration";
+}
