5dea31afdfa30200bf2ac3fa31200652b04b0366
donny <doniwahyu14@gmail.com>
2024-10-07 13:56:25 +0700

    create class EnumDbdt

---
+++ Enumerations/EnumDbdt.cs
@@ -0,0 +1,11 @@
+namespace databasepmapilearn6.Enumerations;
+
+public enum EnumDbdt
+{
+    STRING,
+    DECIMAL,
+    DATE,
+    BOOL,
+    INT,
+    INTTOYEAR
+}
--- ExtensionMethods/ExtIQueryable.cs
+++ ExtensionMethods/ExtIQueryable.cs
@@ -2,5 +2,22 @@ namespace databasepmapilearn6.ExtensionMethods;
 
 public static class ExtIQueryable
 {
+    private const decimal DecimalPrecision = 0.005m;
 
+    public static IQueryable<T> DynamicSearch<T>(this IQueryable<T> query, Dictionary<string, string> search, List<ColumnMapping> columnMappings)
+    {
+        return null;
+    }
+
+
+    #region Helper
+    // helper
+
+    public class ColumnMapping
+    {
+        public string viewColumnName { get; set; }
+        public string dbColumnName { get; set; }
+        // public EnumDb
+    }
+    #endregion
 }
